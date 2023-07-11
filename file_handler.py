6d60f629c7c18f552f48ed82f6832db19c2220f7
miriamYom <m0527669018@gmail.com>
2023-07-11 15:35:08 +0300

    !'

---
+++ file_handler.py
@@ -0,0 +1,29 @@
+import os
+class FileHandler:
+    base_path = None
+    working_directory = None
+
+    @staticmethod
+    def create_dir(path):
+        pass
+
+    @classmethod
+    def find_base_path(cls):
+        if cls.base_path:
+            return cls.base_path
+        # TODO: find first dir's path with .wit in it
+        found = False
+        # TODO: handle not wit repo
+        # raise Exception("Not a wit repository")
+
+    @classmethod
+    def validate_path(cls, path):
+        full_path = os.path.join(cls.working_directory, path)
+        if not os.path.exists(full_path):
+            pass
+            # TODO: handle file doesn't exist
+
+    @classmethod
+    def copy_item(cls, origin, target):
+        pass
+
